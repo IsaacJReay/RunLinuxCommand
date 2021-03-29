ada7a58fc9fb41e62cad4531a23391e1ef45b3af
IsaacJReay <isaacjacksonreay@gmail.com>
2021-03-30 00:32:33 +0700

    call function from another file and add comment for future hint

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
-pub fn ls(arguments: &str) -> (i32, String, String){
-    let options = run_script::ScriptOptions::new();
-    let (code, output, error) = run_script::run_script!(
-        &format!("ls -lah {}", arguments),
-        &vec![],
-        &options
-    ).unwrap();
-
-    (code, output, error)
-}
-
+#[allow(non_snake_case)]
+mod funct;
 fn main() {
-    let path: &str = "/usr/bin";
-    let (code, output, error) = ls(path);
+    
+    // Give Path Arguments for ls command
+    let path: &str = "/home";
+
+    //run foreground command with reading thorough output
+    let (code, output, error) = funct::ls(path);
 
     match &code {
         1 => println!("{}", &error),
         0 => println!("{}", &output),
+        _ => println!("Broken"),
     }
 
+    // run background command with PID returning
+    println!("{}",funct::backgroundls(path));
 
 }
