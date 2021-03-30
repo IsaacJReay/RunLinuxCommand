8245f5672f4408b641126f5bbf5a7e23a2440f70
IsaacJReay <isaacjacksonreay@gmail.com>
2021-03-30 12:00:54 +0700

    use sudo

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-#[allow(non_snake_case)]
 mod funct;
 
 fn main() {
     
     // Give Path Arguments for ls command
-    let path: &str = "/home";
+    let path: &str = "/test";
 
     //run foreground command with reading thorough output
+    let (code, output, error) = funct::sudomkdir(path);
+
+    match &code {
+        1 => println!("{}", &error),
+        0 => println!("{}", &output),
+        _ => println!("Broken"),
+    }
+
     let (code, output, error) = funct::ls(path);
 
     match &code {
@@ -14,6 +21,7 @@ fn main() {
         0 => println!("{}", &output),
         _ => println!("Broken"),
     }
+
 
     // run background command with PID returning
     println!("{}",funct::backgroundls(path));
