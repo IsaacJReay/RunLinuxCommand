f996697aa1f06e9749ea77b2ede84e35fd022813
IsaacJReay <isaacjacksonreay@gmail.com>
2021-03-30 12:03:59 +0700

    add raw string

--- src/funct.rs
+++ src/funct.rs
@@ -12,7 +12,7 @@ pub fn sudomkdir(path: &str) -> (i32, String, String){
 pub fn ls(path: &str) -> (i32, String, String){
     let options = run_script::ScriptOptions::new();
     let (code, output, error) = run_script::run_script!(
-        &format!("ls -lah {}", path),
+        &format!(r#"ls -lah {}"#, path),
         &vec![],
         &options
     ).unwrap();
@@ -23,7 +23,7 @@ pub fn ls(path: &str) -> (i32, String, String){
 pub fn backgroundls(path: &str) -> String{
     let options = run_script::ScriptOptions::new();
     let _child = run_script::spawn_script!(
-        &format!("ls -lah {}", path),
+        &format!(r#"ls -lah {}"#, path),
         &vec![],
         &options
     ).unwrap();
