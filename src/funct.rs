83fda787b1bd8ad9db694dcfaf483300e9e9e34b
IsaacJReay <isaacjacksonreay@gmail.com>
2021-04-06 12:51:50 +0700

    argument replacement in raw string

--- src/funct.rs
+++ src/funct.rs
@@ -1,7 +1,9 @@
 pub fn sudomkdir(path: &str) -> (i32, String, String){
     let options = run_script::ScriptOptions::new();
+    let _command = r#"sudo mkdir path"#;
+    let command = _command.replace("path", path);
     let (code, output, error) = run_script::run_script!(
-        &format!(r#"sudo mkdir {}"#, path),
+        &format!("{}", command),
         &vec![],
         &options
     ).unwrap();
@@ -11,8 +13,10 @@ pub fn sudomkdir(path: &str) -> (i32, String, String){
 
 pub fn ls(path: &str) -> (i32, String, String){
     let options = run_script::ScriptOptions::new();
+    let _command = r#"ls -lah path"#;
+    let command = _command.replace("path", path);
     let (code, output, error) = run_script::run_script!(
-        &format!(r#"ls -lah {}"#, path),
+        &format!("{}", command),
         &vec![],
         &options
     ).unwrap();
@@ -22,8 +26,10 @@ pub fn ls(path: &str) -> (i32, String, String){
 
 pub fn backgroundls(path: &str) -> String{
     let options = run_script::ScriptOptions::new();
+    let _command = r#"ls -lah path"#;
+    let command = _command.replace("path", path);
     let _child = run_script::spawn_script!(
-        &format!(r#"ls -lah {}"#, path),
+        &format!("{}", command),
         &vec![],
         &options
     ).unwrap();
